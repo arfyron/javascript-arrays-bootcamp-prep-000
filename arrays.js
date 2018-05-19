d4b7965e9904748494047665cc8a82302d86798d
Ari Efron <ari.efron@gmail.com>
2018-05-19 20:09:58 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,10 @@
+function addElementToBeginningOfArray(array, element) {
+  newArray = [element, ...array]
+  return newArray
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array = [element, ...array]
+  return array
+}
+
